c484c9dc17c4c85878beb75a0024af740e9b27a3
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2016-06-15 00:45:17 +0530

    more formatted code for blog

--- test.c
+++ test.c
@@ -4,54 +4,39 @@
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 #include <vdpau/vdpau_x11.h>
-
-Display *dis;
-int screen;
-Window win;
-GC gc;
-
+/*------------------X-variables----------------*/
+static Display                           *dis;
+static int                               screen;
+static Window                            win;
+static GC                                gc;
+/*-----------------VDPAU-variables-----------------*/
 static VdpOutputSurface                   output_surface;
 static VdpVideoSurface                    video_surface;
 static VdpPresentationQueueStatus         status;
 static VdpProcamp                         procamp;
-
 static VdpVideoMixer                      video_mixer;
-static float                              min_luma;
-static float                              max_luma;
-static int                                lumakey;
-static int                                bicubic;
 static uint32_t                           vid_width, vid_height;
-static float                              sharpen;
 static VdpChromaType                      vdp_chroma_type;
 static VdpDevice                          vdp_device;
 static VdpYCbCrFormat                     vdp_pixel_format;
-
-
-static VdpGetProcAddress                 *vdp_get_proc_address;
 static int                                colorspace; 
-
 static VdpPresentationQueueTarget         vdp_target;
 static VdpPresentationQueue               vdp_queue;
 
+
+/*-----------------VDPAU-functions-------------------*/
+static VdpGetProcAddress                 *vdp_get_proc_address;
+
 static VdpDeviceDestroy                  *vdp_device_destroy;
+
 static VdpVideoSurfaceCreate             *vdp_video_surface_create;
 static VdpVideoSurfaceDestroy            *vdp_video_surface_destroy;
-
-static VdpGetErrorString                 *vdp_get_error_string;
-
-/* May be used in software filtering/postprocessing options
- * in MPlayer (./mplayer -vf ..) if we copy video_surface data to
- * system memory.
- */
 static VdpVideoSurfacePutBitsYCbCr       *vdp_video_surface_put_bits_y_cb_cr;
-static VdpOutputSurfacePutBitsNative     *vdp_output_surface_put_bits_native;
-static VdpOutputSurfaceGetBitsNative     *vdp_output_surface_get_bits_native;
 
+static VdpOutputSurfaceGetBitsNative     *vdp_output_surface_get_bits_native;
 static VdpOutputSurfaceCreate            *vdp_output_surface_create;
 static VdpOutputSurfaceDestroy           *vdp_output_surface_destroy;
-static VdpOutputSurfaceGetParameters     *vdp_output_surface_get_parameters;
 
-/* VideoMixer puts video_surface data on displayable output_surface. */
 static VdpVideoMixerCreate               *vdp_video_mixer_create;
 static VdpVideoMixerDestroy              *vdp_video_mixer_destroy;
 static VdpVideoMixerRender               *vdp_video_mixer_render;
@@ -62,30 +47,17 @@ static VdpPresentationQueueTargetDestroy *vdp_presentation_queue_target_destroy;
 static VdpPresentationQueueCreate        *vdp_presentation_queue_create;
 static VdpPresentationQueueDestroy       *vdp_presentation_queue_destroy;
 static VdpPresentationQueueDisplay       *vdp_presentation_queue_display;
-static VdpPresentationQueueBlockUntilSurfaceIdle *vdp_presentation_queue_block_until_surface_idle;
 static VdpPresentationQueueTargetCreateX11       *vdp_presentation_queue_target_create_x11;
-static VdpPresentationQueueSetBackgroundColor    *vdp_presentation_queue_set_background_color;
-static VdpPresentationQueueGetTime          *vdp_presentation_queue_get_time;
-static VdpPresentationQueueQuerySurfaceStatus          *vdp_presentation_queue_query_surface_status;
-
-static VdpOutputSurfaceRenderOutputSurface       *vdp_output_surface_render_output_surface;
-static VdpOutputSurfacePutBitsIndexed            *vdp_output_surface_put_bits_indexed;
-static VdpOutputSurfaceRenderBitmapSurface       *vdp_output_surface_render_bitmap_surface;
-
-static VdpBitmapSurfaceCreate                    *vdp_bitmap_surface_create;
-static VdpBitmapSurfaceDestroy                   *vdp_bitmap_surface_destroy;
-static VdpBitmapSurfacePutBitsNative             *vdp_bitmap_surface_putbits_native;
 
 static VdpDecoderCreate                          *vdp_decoder_create;
 static VdpDecoderDestroy                         *vdp_decoder_destroy;
 static VdpDecoderRender                          *vdp_decoder_render;
 
 static VdpGenerateCSCMatrix                      *vdp_generate_csc_matrix;
-static VdpPreemptionCallbackRegister             *vdp_preemption_callback_register;
 
-
-
-
+/*
+ * Intialize the X-Window
+ */
 void init_x() {
 	unsigned long black,white;
 
@@ -93,11 +65,11 @@ void init_x() {
    	screen=DefaultScreen(dis);
 	black=BlackPixel(dis,screen),
 	white=WhitePixel(dis, screen);
-        win=XCreateSimpleWindow(dis,DefaultRootWindow(dis),0,0,	
-                610, 610, 5, black, white);
-        XSetStandardProperties(dis,win,"Howdy","Hi",None,NULL,0,NULL);
+        win=XCreateSimpleWindow(dis,DefaultRootWindow(dis),0,0,
+                300, 300, 5, black, white);
+        XSetStandardProperties(dis,win,"VDPAU Player","VDPAU",None,NULL,0,NULL);
         XSelectInput(dis, win, ExposureMask|ButtonPressMask|KeyPressMask);
-        gc=XCreateGC(dis, win, 0,NULL);        
+        gc=XCreateGC(dis, win, 0,NULL);
         XSetBackground(dis,gc,white);
         XSetBackground(dis,gc,black);
         XClearWindow(dis, win);
@@ -106,12 +78,15 @@ void init_x() {
 void close_x() {
 	XFreeGC(dis, gc);
 	XDestroyWindow(dis,win);
-	XCloseDisplay(dis);	
-	exit(1);				
+	XCloseDisplay(dis);
+	exit(0);
 };
 
+/*
+ * intialize VdpDevice and get pointer to required functions
+ */
 int init_vdpau() {
-    
+
     VdpStatus vdp_st;
 
     struct vdp_function {
@@ -122,12 +97,10 @@ int init_vdpau() {
     const struct vdp_function *dsc;
 
     static const struct vdp_function vdp_func[] = {
-        {VDP_FUNC_ID_GET_ERROR_STRING,                  &vdp_get_error_string},
         {VDP_FUNC_ID_DEVICE_DESTROY,                    &vdp_device_destroy},
         {VDP_FUNC_ID_VIDEO_SURFACE_CREATE,              &vdp_video_surface_create},
         {VDP_FUNC_ID_VIDEO_SURFACE_DESTROY,             &vdp_video_surface_destroy},
         {VDP_FUNC_ID_VIDEO_SURFACE_PUT_BITS_Y_CB_CR,    &vdp_video_surface_put_bits_y_cb_cr},
-        {VDP_FUNC_ID_OUTPUT_SURFACE_PUT_BITS_NATIVE,    &vdp_output_surface_put_bits_native},
         {VDP_FUNC_ID_OUTPUT_SURFACE_GET_BITS_NATIVE,    &vdp_output_surface_get_bits_native},
         {VDP_FUNC_ID_OUTPUT_SURFACE_CREATE,             &vdp_output_surface_create},
         {VDP_FUNC_ID_OUTPUT_SURFACE_DESTROY,            &vdp_output_surface_destroy},
@@ -140,38 +113,22 @@ int init_vdpau() {
         {VDP_FUNC_ID_PRESENTATION_QUEUE_CREATE,         &vdp_presentation_queue_create},
         {VDP_FUNC_ID_PRESENTATION_QUEUE_DESTROY,        &vdp_presentation_queue_destroy},
         {VDP_FUNC_ID_PRESENTATION_QUEUE_DISPLAY,        &vdp_presentation_queue_display},
-        {VDP_FUNC_ID_PRESENTATION_QUEUE_GET_TIME,       &vdp_presentation_queue_get_time},
-        {VDP_FUNC_ID_PRESENTATION_QUEUE_QUERY_SURFACE_STATUS,       &vdp_presentation_queue_query_surface_status},
-        {VDP_FUNC_ID_PRESENTATION_QUEUE_BLOCK_UNTIL_SURFACE_IDLE,
-                        &vdp_presentation_queue_block_until_surface_idle},
         {VDP_FUNC_ID_PRESENTATION_QUEUE_TARGET_CREATE_X11,
                         &vdp_presentation_queue_target_create_x11},
-        {VDP_FUNC_ID_PRESENTATION_QUEUE_SET_BACKGROUND_COLOR,
-                        &vdp_presentation_queue_set_background_color},
-        {VDP_FUNC_ID_OUTPUT_SURFACE_RENDER_OUTPUT_SURFACE,
-                        &vdp_output_surface_render_output_surface},
-        {VDP_FUNC_ID_OUTPUT_SURFACE_PUT_BITS_INDEXED,   &vdp_output_surface_put_bits_indexed},
         {VDP_FUNC_ID_DECODER_CREATE,                    &vdp_decoder_create},
         {VDP_FUNC_ID_DECODER_RENDER,                    &vdp_decoder_render},
         {VDP_FUNC_ID_DECODER_DESTROY,                   &vdp_decoder_destroy},
-        {VDP_FUNC_ID_BITMAP_SURFACE_CREATE,             &vdp_bitmap_surface_create},
-        {VDP_FUNC_ID_BITMAP_SURFACE_DESTROY,            &vdp_bitmap_surface_destroy},
-        {VDP_FUNC_ID_BITMAP_SURFACE_PUT_BITS_NATIVE,    &vdp_bitmap_surface_putbits_native},
-        {VDP_FUNC_ID_OUTPUT_SURFACE_RENDER_BITMAP_SURFACE,
-                        &vdp_output_surface_render_bitmap_surface},
-        {VDP_FUNC_ID_OUTPUT_SURFACE_GET_PARAMETERS,     &vdp_output_surface_get_parameters},
         {VDP_FUNC_ID_GENERATE_CSC_MATRIX,               &vdp_generate_csc_matrix},
-        {VDP_FUNC_ID_PREEMPTION_CALLBACK_REGISTER,      &vdp_preemption_callback_register},
         {0, NULL}
     };
+
     vdp_st = vdp_device_create_x11(dis, screen,
                                &vdp_device, &vdp_get_proc_address);
-    
+
     if (vdp_st != VDP_STATUS_OK) {
         return -1;
     }
 
-    vdp_get_error_string = NULL;
     for (dsc = vdp_func; dsc->pointer; dsc++) {
         vdp_st = vdp_get_proc_address(vdp_device, dsc->id, dsc->pointer);
         if (vdp_st != VDP_STATUS_OK) {
@@ -182,28 +139,29 @@ int init_vdpau() {
     return 0;
 }
 
-
+/*
+ * intialize VdpPresentationQueue
+ */
 int init_vdpau_queue()
 {
     VdpStatus vdp_st;
-    // {0, 0, 0, 0} makes the video shine through any black window on top
-    VdpColor vdp_bg = {0.0, 0.0, 0.0, 1};
 
     vdp_st = vdp_presentation_queue_target_create_x11(vdp_device, win,
                                                       &vdp_target);
     if(vdp_st != VDP_STATUS_OK) return -1;
+
     XMapRaised(dis, win);
 
     vdp_st = vdp_presentation_queue_create(vdp_device, vdp_target,
                                            &vdp_queue);
     if(vdp_st != VDP_STATUS_OK) return -1;
 
-    vdp_st = vdp_presentation_queue_set_background_color(vdp_queue, &vdp_bg);
-    if(vdp_st != VDP_STATUS_OK) return -1;
- 
     return 0;
 }
 
+/*
+ * Set the Color Space Conversion Matrix
+ */
 static int update_csc_matrix(void)
 {
     VdpStatus vdp_st;
@@ -217,15 +175,20 @@ static int update_csc_matrix(void)
     if (!csp)
         csp = vid_width >= 1280 || vid_height > 576 ? 2 : 1;
 
-
     vdp_st = vdp_generate_csc_matrix(&procamp, vdp_colors[csp], &matrix);
-    
+
+    if(vdp_st != VDP_STATUS_OK) return -1;
+
     vdp_st = vdp_video_mixer_set_attribute_values(video_mixer, 1, attributes,
                                                   attribute_values);
+    if(vdp_st != VDP_STATUS_OK) return -1;
+ 
     return 0;
 }
 
-
+/*
+ * initalize VdpVideoMixer
+ */
 static int create_vdp_mixer(VdpChromaType vdp_chroma_type)
 {
 #define VDP_NUM_MIXER_PARAMETER 3
@@ -233,95 +196,99 @@ static int create_vdp_mixer(VdpChromaType vdp_chroma_type)
     int i;
     VdpStatus vdp_st;
     int feature_count = 0;
-   
+
     VdpVideoMixerFeature features[MAX_NUM_FEATURES];        
     VdpBool feature_enables[MAX_NUM_FEATURES];
-    static const VdpVideoMixerAttribute sharpen_attrib[] = {VDP_VIDEO_MIXER_ATTRIBUTE_SHARPNESS_LEVEL};
-    const void * const sharpen_value[] = {&sharpen};
-    static const VdpVideoMixerAttribute lumakey_attrib[] = {VDP_VIDEO_MIXER_ATTRIBUTE_LUMA_KEY_MIN_LUMA, VDP_VIDEO_MIXER_ATTRIBUTE_LUMA_KEY_MAX_LUMA};
-    const void * const lumakey_value[] = {&min_luma, &max_luma};
+
     static const VdpVideoMixerParameter parameters[VDP_NUM_MIXER_PARAMETER] = {
         VDP_VIDEO_MIXER_PARAMETER_VIDEO_SURFACE_WIDTH,
         VDP_VIDEO_MIXER_PARAMETER_VIDEO_SURFACE_HEIGHT,
         VDP_VIDEO_MIXER_PARAMETER_CHROMA_TYPE
     };
+
     const void *const parameter_values[VDP_NUM_MIXER_PARAMETER] = {
         &vid_width,
         &vid_height,
         &vdp_chroma_type
     };
-    if (lumakey)
-        features[feature_count++] = VDP_VIDEO_MIXER_FEATURE_LUMA_KEY;
-    if (sharpen)
-        features[feature_count++] = VDP_VIDEO_MIXER_FEATURE_SHARPNESS;
-    if (bicubic)
-        features[feature_count++] = VDP_VIDEO_MIXER_FEATURE_HIGH_QUALITY_SCALING_L1;
 
     vdp_st = vdp_video_mixer_create(vdp_device, feature_count, features,
                                     VDP_NUM_MIXER_PARAMETER,
                                     parameters, parameter_values,
                                     &video_mixer);
 
+    if(vdp_st != VDP_STATUS_OK) return -1;
 
-    for (i = 0; i < feature_count; i++)
-        feature_enables[i] = VDP_TRUE;
-    if (feature_count)
-        vdp_video_mixer_set_feature_enables(video_mixer, feature_count, features, feature_enables);
-    if (lumakey)
-        vdp_video_mixer_set_attribute_values(video_mixer, 2, lumakey_attrib, lumakey_value);
-    if (sharpen)
-        vdp_video_mixer_set_attribute_values(video_mixer, 1, sharpen_attrib, sharpen_value);
-  update_csc_matrix();
-    return 0;
-
+    return update_csc_matrix();
 }
 
+/*
+ * Create a Video Surface
+ */
 int create_video_surface(){
-     
+ 
     VdpStatus vdp_st;
 
     vdp_st = vdp_video_surface_create(vdp_device, vdp_chroma_type,
                                            vid_width, vid_height,
                                            &video_surface);
-    if (vdp_st == VDP_STATUS_OK) return 1;
-    else return 0;
+
+    if (vdp_st != VDP_STATUS_OK) return -1;
+
+    return 0;
 }
 
+/*
+ * create a VdpOutputSurface
+ */
 int create_output_surface(){
-     
+ 
     VdpStatus vdp_st;
 
     vdp_st = vdp_output_surface_create(vdp_device, VDP_RGBA_FORMAT_B8G8R8A8,
-                                           vid_width*1.9, vid_height*1.9,
+                                           vid_width, vid_height,
                                            &output_surface);
-    if (vdp_st == VDP_STATUS_OK) return 1;
-    else return 0;
+
+    if (vdp_st != VDP_STATUS_OK) return -1;
+
+    return 0;
 }
 
+/*
+ * put frame data on VdpVideoSurface
+ */
 int put_bits() {
     uint32_t **data;
     int i,j;
     const uint32_t a[1] = {vid_width*4};
     VdpStatus vdp_st;
-    
+
     data = (uint32_t * * )calloc(1, sizeof(uint32_t *));
-    
-    for(i = 0; i < 1; i++) data[i] = (uint32_t *)calloc(vid_width*vid_height, sizeof(uint32_t *));
-    
+
+    for(i = 0; i < 1; i++)
+        data[i] = (uint32_t *)calloc(vid_width*vid_height, sizeof(uint32_t *));
+ 
     for(i = 0; i < 1; i++) {
-        for(j = 0; j < vid_width*vid_height; j++) scanf("%ld",&data[i][j]);
+        for(j = 0; j < vid_width*vid_height; j++)
+            scanf("%ld", &data[i][j]);
     }
 
     vdp_st = vdp_video_surface_put_bits_y_cb_cr(video_surface, vdp_pixel_format,
                                                 (void const*const*)data,
                                                  a);
+ 
     for (i = 0; i < 1; i++) {
         free(data[i]);
     }
-    return 0; 
 
+    if (vdp_st != VDP_STATUS_OK) return -1;
+
+    return 0;
 }
 
+/*
+ * retrieve data from VdpOutputSurface
+ */
 int get_bits() {
     uint32_t **data;
     int i,j;
@@ -330,37 +297,26 @@ int get_bits() {
     
     data = (uint32_t * * )calloc(1, sizeof(uint32_t *));
     
-    for(i = 0; i < 1; i++) data[i] = (uint32_t *)calloc(vid_width*vid_height, sizeof(uint32_t *));
+    for(i = 0; i < 1; i++)
+        data[i] = (uint32_t *)calloc(vid_width*vid_height, sizeof(uint32_t *));
     
     vdp_st = vdp_output_surface_get_bits_native(output_surface,NULL,
                                                 (void * const*)data,
                                                  a);
  
-    for(i = 0; i < vid_height; i++){
-        for(j = 0; j < vid_width; j++){
-            printf("%ld " ,data[0][i*vid_width+j]/16777216);
-        }
-        printf("\n");
-    }
+    if (vdp_st != VDP_STATUS_OK) return -1;
 
     return 0; 
-
 }
 
 int main(){
+    KeySym key;
+    XEvent event;
+    
     vdp_chroma_type = VDP_CHROMA_TYPE_420;
     vid_width = 300;
     vid_height = 300;
-    char text[255];
-    KeySym key;
-    XEvent event;
-    uint64_t tim = 0;
     colorspace =1;  
-    sharpen = 0;
-    lumakey = 0;
-    bicubic = 1;
-    min_luma = 0.600000;
-    max_luma = 0.900000;
     VdpStatus vdp_st;
     int field = VDP_VIDEO_MIXER_PICTURE_STRUCTURE_FRAME;
     vdp_pixel_format = VDP_YCBCR_FORMAT_Y8U8V8A8;
@@ -370,14 +326,35 @@ int main(){
     procamp.saturation = 1.0;
     procamp.hue        = 0.0;
     uint32_t rgba_format = 5;
+    int status;
 
     init_x();
-    init_vdpau();
-    init_vdpau_queue();    
-    create_vdp_mixer(vdp_chroma_type);
-    create_video_surface();
-    create_output_surface();
-    put_bits();
+
+    status = init_vdpau();
+    if(status == -1)
+        printf("Error in initializing VdpDevice\n");
+
+    status = create_video_surface();
+    if(status == -1)
+        printf("Error in creating VdpVideoSurface\n");
+
+    status = create_output_surface();
+    if(status == -1)
+        printf("Error in creating VdpOutputSurface\n");
+
+    status = create_vdp_mixer(vdp_chroma_type);
+    if(status == -1)
+        printf("Error in creating VdpVideoMixer\n");
+
+    status = init_vdpau_queue();    
+    if(status == -1)
+        printf("Error in initializing VdpPresentationQueue\n");
+
+    status = put_bits();
+    if(status == -1)
+        printf("Error in Putting data on VdpVideoSurface\n");
+
+   
     vdp_st = vdp_video_mixer_render(video_mixer, VDP_INVALID_HANDLE, 0,
                                         field, 0, (VdpVideoSurface*)VDP_INVALID_HANDLE,
                                         video_surface,
@@ -388,14 +365,21 @@ int main(){
     
 
     get_bits();
-    while(1) 
-
+    int i = 500;
+    while(i) 
     {
            vdp_st = vdp_presentation_queue_display(vdp_queue,
                                                   output_surface, 
                                                   0, 0,
                                                   0);
+           i--;
     }
 
-    return 0;
+    while(1){
+       XNextEvent(dis, &event);
+
+       if(event.type == KeyPress)
+           close_x();
+    }
+    
 }
